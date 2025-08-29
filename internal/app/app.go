2415bb40842626762986f2967e0abcd97c8c1caa
ReanGD <reangd@yandex.ru>
2025-08-29 03:01:07 +0300

    apply config params for server

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,31 +4,19 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/LumivoxAI/webrelay/internal/config"
 	"github.com/LumivoxAI/webrelay/internal/httpapi"
 	"go.uber.org/zap"
 )
 
-// Config is a temporary runtime configuration until YAML loading is implemented.
-type Config struct {
-	Listen         string
-	RequestTimeout time.Duration
-}
-
-func DefaultConfig() Config {
-	return Config{
-		Listen:         "127.0.0.1:8080",
-		RequestTimeout: 35 * time.Second,
-	}
-}
-
 // NewServer creates the HTTP server with the base public API pipeline.
-func NewServer(config Config, logger *zap.Logger) *http.Server {
+func NewServer(config config.Config, logger *zap.Logger) *http.Server {
 	return &http.Server{
-		Addr:              config.Listen,
+		Addr:              config.Server.Listen,
 		Handler:           httpapi.NewHandler(logger),
 		ReadHeaderTimeout: 5 * time.Second,
-		ReadTimeout:       config.RequestTimeout,
-		WriteTimeout:      config.RequestTimeout,
+		ReadTimeout:       config.Server.RequestTimeout.Std(),
+		WriteTimeout:      config.Server.RequestTimeout.Std(),
 		IdleTimeout:       60 * time.Second,
 	}
 }
